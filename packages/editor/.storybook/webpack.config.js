a30b6beac31f9d46f1c033a9ece707d1e688329a
Stephen Fraser <stephenwf@users.noreply.github.com>
2020-02-18 13:10:42 +0000

    Fixed storybook (#4)

--- packages/editor/.storybook/webpack.config.js
+++ packages/editor/.storybook/webpack.config.js
@@ -1,8 +1,38 @@
-const config = require('@fesk/scripts/lib/scripts/parts/storybook/react/webpack.config');
+const webpack = require('webpack');
+const path = require('path');
+const SRC_PATH = path.join(process.cwd(), './src');
 
-module.exports = (props) => {
+module.exports = ({ config }) => {
 
-  props.config.module.rules.push({
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env.SRC_PATH': JSON.stringify(SRC_PATH),
+    })
+  );
+
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx|js|jsx)$/,
+      exclude: /(node_modules)/,
+      include: [SRC_PATH],
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [[require('@fesk/babel-config'), { typescript: true }]],
+          },
+        },
+      ],
+    },
+    {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: SRC_PATH,
+    }
+  );
+  config.resolve.extensions.push('.jss', '.jsx', '.scss', '.ts', '.tsx');
+
+  config.module.rules.push({
     test: /\.stories\.tsx?$/,
     loaders: [
       {
@@ -13,5 +43,5 @@ module.exports = (props) => {
     enforce: 'pre',
   });
 
-  return config(props);
+  return config;
 };
