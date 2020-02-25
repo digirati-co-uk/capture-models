c638f0b3a5c9fce420aee561c4d54201c2e2e2ae
stephenwf <stephen.fraser@digirati.com>
2020-02-25 11:21:27 +0000

    Added IDs to capture models with revisions

---
+++ packages/database/src/utility/document-to-inserts.ts
@@ -0,0 +1,50 @@
+import { traverseDocument } from '@capture-models/editor/lib/utility/traverse-document';
+import { CaptureModel as CaptureModelType } from '@capture-models/types';
+import { Document } from '../entity/Document';
+import { Field } from '../entity/Field';
+import { Property } from '../entity/Property';
+import { fromDocument } from '../mapping/from-document';
+import { fromField } from '../mapping/from-field';
+
+export function documentToInserts(
+  document: CaptureModelType['document'],
+  parentDocument?: { id: string; term: string },
+  rootDocumentId?: string
+) {
+  const dbInserts: (Field | Document | Property)[][] = [];
+  traverseDocument<{ parentAdded?: boolean }>(document, {
+    beforeVisitEntity(entity, term, parent) {
+      const entityDoc = fromDocument(entity, false);
+      if (parent) {
+        entityDoc.parentId = `${parent.id}/${term}`;
+      } else if (parentDocument) {
+        entityDoc.parentId = `${parentDocument.id}/${parentDocument.term}`;
+      }
+      dbInserts.push([entityDoc]);
+      dbInserts.push(
+        entityDoc.properties.map(prop => {
+          if (rootDocumentId) {
+            prop.rootDocumentId = rootDocumentId;
+          } else if (parentDocument) {
+            prop.rootDocumentId = parentDocument.id;
+          } else {
+            prop.rootDocumentId = document.id;
+          }
+          return prop;
+        })
+      );
+      const fieldInserts = [];
+      entityDoc.properties.forEach(prop => {
+        entity.properties[prop.term].forEach(field => {
+          if (field.type !== 'entity') {
+            const fieldToSave = fromField(field);
+            fieldToSave.parentId = `${entityDoc.id}/${prop.term}`;
+            fieldInserts.push(fieldToSave);
+          }
+        });
+      });
+      dbInserts.push(fieldInserts);
+    },
+  });
+  return dbInserts;
+}
---
+++ packages/editor/src/utility/create-revision-request.ts
@@ -0,0 +1,59 @@
+import { CaptureModel, Revision, RevisionRequest } from '@capture-models/types';
+import { filterCaptureModel } from './filter-capture-model';
+import { expandModelFields } from '../core/structure-editor';
+
+export function createRevisionRequestFromStructure(
+  captureModel: CaptureModel,
+  structure: CaptureModel['structure']
+): RevisionRequest {
+  if (structure.type !== 'model') {
+    throw new Error('Cannot make revision from choice');
+  }
+
+  const flatFields = expandModelFields(structure.fields);
+  const structureDocument = filterCaptureModel(structure.id, captureModel.document, flatFields, field => {
+    return !field.revision; // Where there is no revision.
+  });
+
+  if (!structureDocument) {
+    throw new Error(`Invalid structure ${structure.id} (${structure.label})`);
+  }
+
+  return {
+    captureModelId: captureModel.id,
+    revision: {
+      id: structure.id,
+      fields: structure.fields,
+      approved: true,
+      structureId: structure.id,
+      label: structure.label,
+    },
+    modelRoot: structure.modelRoot,
+    source: 'canonical',
+    document: structureDocument,
+  };
+}
+
+export function createRevisionRequest(
+  captureModel: CaptureModel,
+  revision: Revision,
+  inputDocument?: CaptureModel['document']
+): RevisionRequest {
+  const flatFields = expandModelFields(revision.fields);
+  const document = inputDocument
+    ? inputDocument
+    : filterCaptureModel(revision.id, captureModel.document, flatFields, field => {
+        return field.revision ? field.revision === revision.id : false;
+      });
+
+  if (!document) {
+    throw new Error(`Invalid revision ${revision.id} has no document`);
+  }
+
+  return {
+    captureModelId: captureModel.id,
+    revision,
+    document,
+    source: revision.structureId ? 'structure' : 'unknown',
+  };
+}
