b730e04e270aa97247d528b20af8a7c0d5c85443
Stephen Fraser <stephen.fraser@digirati.com>
2020-02-26 11:02:43 +0000

    Added drop helper

---
+++ packages/server/bin/drop.js
@@ -0,0 +1,20 @@
+const { CaptureModelDatabase } = require('@capture-models/database');
+const nodemonConfig = require('../nodemon');
+
+async function main() {
+  const db = await CaptureModelDatabase.create({
+    host: nodemonConfig.env.DATABASE_HOST,
+    port: nodemonConfig.env.DATABASE_PORT ? +nodemonConfig.env.DATABASE_PORT : 5432,
+    name: 'default',
+    username: nodemonConfig.env.DATABASE_USER,
+    password: nodemonConfig.env.DATABASE_PASSWORD,
+    database: nodemonConfig.env.DATABASE_NAME,
+    schema: nodemonConfig.env.DATABASE_SCHEMA ? nodemonConfig.env.DATABASE_SCHEMA : 'public',
+    synchronize: nodemonConfig.env.NODE_ENV === 'development',
+  });
+  await db.drop();
+}
+
+main().catch(err => {
+  throw err;
+});
