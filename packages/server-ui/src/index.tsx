34c6cfb25c448b402df4bc0f7aa00cfddd58209e
stephenwf <stephen.fraser@digirati.com>
2020-02-27 08:15:18 +0000

    Added routes to Server UI

--- packages/server-ui/src/index.tsx
+++ packages/server-ui/src/index.tsx
@@ -3,12 +3,15 @@ import { render } from 'react-dom';
 import { defaultTheme } from '@capture-models/editor';
 import 'semantic-ui-css/semantic.min.css';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { RootExamples } from './RootExamples';
 import './layout.scss';
 
 render(
   <ThemeProvider theme={defaultTheme}>
-    <RootExamples />
+    <Router>
+      <RootExamples />
+    </Router>
   </ThemeProvider>,
   document.getElementById('root')
 );
