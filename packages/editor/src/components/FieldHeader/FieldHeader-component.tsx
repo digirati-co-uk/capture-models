3cc769fcdd004c28947cdaa9f1893ce999d30f87
stephenwf <stephen.fraser@digirati.com>
2020-03-11 09:52:59 +0000

    Fixed field header selector
    - Fixed CSS class name, broken since BEM library migration
    - Added events for when selector is opened and closed

--- packages/editor/src/components/FieldHeader/FieldHeader-component.tsx
+++ packages/editor/src/components/FieldHeader/FieldHeader-component.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Label } from 'semantic-ui-react';
 import './FieldHeader.scss';
 
@@ -9,6 +9,8 @@ type FieldHeaderProps = {
   term?: string;
   description?: string;
   selectorComponent?: any;
+  onSelectorClose?: () => void;
+  onSelectorOpen?: () => void;
 };
 
 export const FieldHeaderComponent: React.FC<FieldHeaderProps> = ({
@@ -18,8 +20,24 @@ export const FieldHeaderComponent: React.FC<FieldHeaderProps> = ({
   showTerm,
   labelFor,
   label,
+  onSelectorClose,
+  onSelectorOpen,
 }) => {
   const [open, setOpen] = useState(false);
+
+  const toggleSelector = useCallback(() => {
+    if (open) {
+      setOpen(false);
+      if (onSelectorClose) {
+        onSelectorClose();
+      }
+    } else {
+      setOpen(true);
+      if (onSelectorOpen) {
+        onSelectorOpen();
+      }
+    }
+  }, [onSelectorClose, onSelectorOpen, open]);
 
   return (
     <div className={`field-header`}>
@@ -28,16 +46,20 @@ export const FieldHeaderComponent: React.FC<FieldHeaderProps> = ({
           <label className={`field-header__title`} htmlFor={labelFor}>
             {label} {showTerm && term ? <Label size="tiny">{term}</Label> : null}
           </label>
-          {description ? <div className={`field-header__subtitle`}>{description}</div> : null}
+          {description ? (
+            <label className={`field-header__subtitle`} htmlFor={labelFor}>
+              {description}
+            </label>
+          ) : null}
         </div>
         {selectorComponent ? (
-          <div className={`field-header__right`} onClick={() => setOpen(s => !s)}>
-            <div className={`field-header__icon ${open ? 'field-header__icon' : ''}`}>Edit Selector</div>
+          <div className={`field-header__right`} onClick={toggleSelector}>
+            <div className={`field-header__icon ${open ? 'field-header__icon--open' : ''}`}>Edit Selector</div>
           </div>
         ) : null}
       </div>
       {selectorComponent ? (
-        <div className={`field-header__bottom ${open ? 'field-header__bottom' : ''}`}>
+        <div className={`field-header__bottom ${open ? 'field-header__bottom--open' : ''}`}>
           <div className={`field-header__bottom-inner`}>{selectorComponent ? selectorComponent : null}</div>
         </div>
       ) : null}
