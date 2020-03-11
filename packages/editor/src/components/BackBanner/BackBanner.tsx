08a48518f016f4484842d01956d940e033e0ad0e
stephenwf <stephen.fraser@digirati.com>
2020-03-11 09:51:55 +0000

    New BackBanner component

---
+++ packages/editor/src/components/BackBanner/BackBanner.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const BackBanner = styled.div`
+  background: ${props => props.theme.colors.mutedPrimary};
+  color: ${props => props.theme.colors.textOnMutedPrimary};
+  padding: 0.3em 30px;
+`;
