025bf2e2fe5a991a727b00508855c3f772790941
Stephen Fraser <stephen.fraser@digirati.com>
2020-03-13 18:16:11 +0000

    Optimise refinement memoization

--- packages/plugin-api/src/hooks/use-refinement.ts
+++ packages/plugin-api/src/hooks/use-refinement.ts
@@ -4,22 +4,22 @@ import React, { useContext, useMemo } from 'react';
 
 export function useRefinement<Ref extends UnknownRefinement = UnknownRefinement>(
   type: string,
-  subject: { instance: RefinementType<Ref>; property: string },
+  { instance, property }: { instance: RefinementType<Ref>; property: string },
   context: RefinementSupportProps & RefinementContext<Ref>
 ): Ref | null {
   const ctx = useContext(PluginContext);
   const refinements = useMemo(() => ctx.refinements.filter(r => r.type === type), [ctx.refinements, type]) as Ref[];
 
   return useMemo(() => {
-    if (!subject) {
+    if (!instance) {
       return null;
     }
     for (const refinement of refinements) {
-      const match = refinement.supports(subject as any, context as any);
+      const match = refinement.supports({ instance, property } as any, context as any);
       if (match) {
         return refinement;
       }
     }
     return null;
-  }, [context, refinements, subject]);
+  }, [instance, refinements, property, context]);
 }
