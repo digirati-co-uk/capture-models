99e1488a455d45f84d448556d42400cacc3b124b
Stephen Fraser <stephen.fraser@digirati.com>
2020-03-13 10:24:26 +0000

    Updated styling of box selector

--- packages/editor/src/selector-types/BoxSelector/BoxSelector.canvas-panel.tsx
+++ packages/editor/src/selector-types/BoxSelector/BoxSelector.canvas-panel.tsx
@@ -1,11 +1,26 @@
 import { SelectorComponent } from '@capture-models/types';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BoxSelectorProps } from './BoxSelector';
 // @ts-ignore
 import EditableAnnotation from '@canvas-panel/core/es/components/EditableAnnotation/EditableAnnotation';
+import { useViewer } from '../../content-types/CanvasPanel/CanvasPanel';
 
 const BoxSelectorCanvasPanel: SelectorComponent<BoxSelectorProps> = props => {
   const [selector, setSelector] = useState<BoxSelectorProps['state']>(props.state);
+  const viewer = useViewer();
+
+  useEffect(() => {
+    if (viewer && props.state && props.isTopLevel) {
+      const t = setTimeout(() => {
+        viewer.goToRect(props.state, 50);
+      }, 100);
+      return () => {
+        clearTimeout(t);
+        viewer.resetView();
+      };
+    }
+    return () => {};
+  }, [props.isTopLevel, props.state, viewer]);
 
   if (!props.state) return null;
 
