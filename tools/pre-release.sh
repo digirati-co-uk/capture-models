9cef2190eef22a78f80c3f14fc18d28abcefcc3c
Stephen Fraser <stephen.fraser@digirati.com>
2020-03-14 23:15:36 +0000

    Fixed typo

--- tools/pre-release.sh
+++ tools/pre-release.sh
@@ -4,8 +4,8 @@ set -e
 
 DIST_TAG=$1;shift;
 
-yarn lerna publish --canary --yes --preid "$DIST_TAG" --dist-tag "$DIST_TAG"
+yarn lerna publish --force-publish --canary --yes --preid "$DIST_TAG" --dist-tag "$DIST_TAG"
 
-docker built -t digirati/capture-models:"$DIST_TAG" --build-arg PINNED_CAPTURE_MODEL_VERSION="$DIST_TAG" .
+docker build -t digirati/capture-models:"$DIST_TAG" --build-arg PINNED_CAPTURE_MODEL_VERSION="$DIST_TAG" .
 
 docker push digirati/capture-models:"$DIST_TAG"
