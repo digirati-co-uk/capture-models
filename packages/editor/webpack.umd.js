c5cc96f6a594b4940599ee2adfeb464338a253e0
Stephen Fraser <stephen.fraser@digirati.com>
2020-07-13 23:31:49 +0100

    Removed debug

--- packages/editor/webpack.umd.js
+++ packages/editor/webpack.umd.js
@@ -2,8 +2,6 @@ const path = require('path');
 const baseConfig = require('@fesk/scripts/webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MetalsmithWebpackPlugin = require('@fesk/plugin-metalsmith');
-const Visualizer = require('webpack-visualizer-plugin');
-const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
 
 const fullName = '@capture-models/editor';
 const packageName = 'CaptureModelEditor';
@@ -22,9 +20,6 @@ const plugins = baseConfig.plugins
     return plugin;
   })
   .filter(Boolean);
-
-plugins.push(new Visualizer());
-plugins.push(new DuplicatePackageCheckerPlugin());
 
 const config = Object.assign({}, baseConfig, {
   output: {
