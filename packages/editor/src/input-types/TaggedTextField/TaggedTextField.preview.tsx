beb99233c74971dc517e1a53a5805231e8d25931
Stephen Fraser <stephen.fraser@digirati.com>
2020-07-13 21:03:17 +0100

    Improved HTML and tagged template previews

--- packages/editor/src/input-types/TaggedTextField/TaggedTextField.preview.tsx
+++ packages/editor/src/input-types/TaggedTextField/TaggedTextField.preview.tsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { customAlphabet, urlAlphabet } from 'nanoid';
 
-import { TaggedTextFieldProps } from './TaggedTextField';
+import { presets, TagDefinition, TaggedTextFieldProps } from './TaggedTextField';
+import { HTMLPreviewContainer } from '../HTMLField/HTMLField.preview';
 
-export const TaggedTextFieldPreview: React.FC<TaggedTextFieldProps> = ({ value }) => {
+function parseTags(prefix: string, tags: TagDefinition) {
+  if (!tags.backgroundColor && !tags.color) {
+    return '';
+  }
+
+  const selector = tags.isHTML ? tags.tag : `[data-tag="${tags.tag}"]`;
+  return `
+    ${prefix} ${selector} {
+      ${tags.backgroundColor ? `background: ${tags.backgroundColor};` : ''}
+      ${tags.color ? `color: ${tags.color};` : ''}
+    }`;
+}
+
+function parseStyles(prefix: string, { tags = [], blocks = [] }: { tags?: TagDefinition[]; blocks?: TagDefinition[] }) {
+  return `
+    ${tags
+      .map(tag => parseTags(prefix, tag))
+      .filter(e => e)
+      .join('')}
+    ${blocks
+      .map(block => parseTags(prefix, block))
+      .filter(e => e)
+      .join('')}
+  `;
+}
+
+export const TaggedTextFieldPreview: React.FC<TaggedTextFieldProps> = ({ value, tags, blocks, preset }) => {
+  const id = useMemo(() => customAlphabet(urlAlphabet, 10)(), []);
+  const styles = useMemo(() => {
+    return parseStyles(`.tagged-${id}`, preset && presets[preset] ? presets[preset] : { tags, blocks });
+  }, [id, preset, tags, blocks]);
+
   if (!value) {
     return <span style={{ color: '#999' }}>No value</span>;
   }
 
-  return <div dangerouslySetInnerHTML={{ __html: value }} />;
+  return (
+    <>
+      <div className={`tagged-${id}`}>
+        <HTMLPreviewContainer dangerouslySetInnerHTML={{ __html: value }} />
+      </div>
+      <style>{styles}</style>
+    </>
+  );
 };
