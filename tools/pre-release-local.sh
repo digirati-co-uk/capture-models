5b6cfffd44f6b70f5d7289ad2a349e07246bd618
Stephen Fraser <stephen.fraser@digirati.com>
2020-08-07 19:33:59 +0100

    Added local pre-release

--- tools/pre-release-local.sh
+++ tools/pre-release-local.sh
@@ -4,7 +4,5 @@ set -e
 
 DIST_TAG=$1;shift;
 
-yarn lerna publish --force-publish --canary --yes --preid "$DIST_TAG" --dist-tag "$DIST_TAG"
-
-docker build -t digirati/capture-models:"$DIST_TAG" --build-arg PINNED_CAPTURE_MODEL_VERSION="$DIST_TAG" .
+docker build -f Dockerfile.dev -t digirati/capture-models:"$DIST_TAG" .
 
